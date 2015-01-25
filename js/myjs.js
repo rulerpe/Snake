965e571b0d9e74d83c6372374f68d1eabb042d5c
Peter <neetisthebest@gmail.com>
2015-01-25 00:31:09 -0800

    editing move function

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,6 @@
+	var mblue = "rgb(0, 0, 255)";
+	var mred = "rgb(255, 0, 0)";
+	var mgray = "rgb(192, 192, 192)";
 $(document).ready(init());
 
 function init(){
@@ -20,18 +23,43 @@ function render(){
 
 function move(){
 	var snake = {
-		"start_pos": null,
-		"end_pos": null,
+		"pos": null,
 		"length": 2,
 		"dir": "r"
 	}
 
+	var temp = null;
+
 	if (snake.pos == null) {
-			snake.start_pos = 50;
-			snake.end_pos = 49;
-			$(".cell").eq(snake.start_pos).css("background-color", "blue");
-			$(".cell").eq(snake.end_pos).css("background-color", "blue");
+			snake.pos = [50,49]
+			$(".cell").eq(snake.pos[0]).css("background-color", mblue);
+			$(".cell").eq(snake.pos[snake.pos.length-1]).css("background-color", mblue);
 		}
+	direction(snake);
 
 	
+}
+
+function direction(obj){
+	var longer;
+	if(obj.dir = "r"){
+		var temp = obj.pos[0] + 1;
+		var color = $(".cell").eq(temp).css("background-color") ;
+		if (temp%20 == 0){
+			return;
+		}else if(color == mblue){
+			return;
+		}else if(color == mgray){
+			longer = false;
+		}else if(color == mred){
+			longer = true;
+		};
+
+		if (longer==false){
+			$(".cell").eq(obj.pos[obj.pos.length-1]).css("background-color", mgray);
+
+		};
+		$(".cell").eq(temp).css("background-color",mblue); 
+
+	}
 }
